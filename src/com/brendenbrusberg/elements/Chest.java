602e8f76eae8a9dcca27653c953f37c3695d6d74
Brenden Brusberg <brenbrus@gmail.com>
2016-12-31 11:39:05 -0500

    lwjgl Dependencies
    Class Path/Also Chest and Status Replace

---
+++ src/com/brendenbrusberg/elements/Chest.java
@@ -0,0 +1,7 @@
+package com.brendenbrusberg.elements;
+
+/**
+ * Created by Brenden on 6/30/2016.
+ */
+public class Chest {
+}
--- src/io/github/bobjoetom/elements/Chest.java
+++
@@ -1,7 +0,0 @@
-package io.github.bobjoetom.elements;
-
-/**
- * Created by Brenden on 6/30/2016.
- */
-public class Chest {
-}
